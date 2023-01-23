cabeeb9002ae401a88330a23d68bc5f9139d0fc2
Indra Rosadi <indra.rosadi@rallyhealth.com>
2023-01-23 09:46:31 -0600

    Test changes

---
+++ push-protect.sh
@@ -0,0 +1,24 @@
+TEMP_BRANCH="push-protect/$RANDOM-$RANDOM-$RANDOM"
+echo "Pushing the current commit to a temporary branch: $TEMP_BRANCH"
+git push -u origin $TEMP_BRANCH
+WAIT_UNTIL=$((`date +%s` + 600))
+while [ true ]
+do
+  echo "Sleeping for 30 seconds"
+  sleep 30
+  echo "Attempting to push to main"
+  if git push origin main; then
+    echo "Push is successful"
+    git push origin --delete $TEMP_BRANCH
+    exit 0
+  else
+    echo "Push to main still failed."
+    CUR_TIME=$((`date +%s`))
+    if [ $CUR_TIME -gt $WAIT_UNTIL ]
+    then
+      echo "Already waited for 600 seconds and still can't push. Exiting with error."
+      git push origin --delete $TEMP_BRANCH
+      exit 1
+    fi
+  fi
+done
